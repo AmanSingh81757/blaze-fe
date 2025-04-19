46507ce01f6cf5a97b58372852c2e3490112584f
tesla59 <nishant@heim.id>
2025-04-20 03:40:08 +0530

    Remove connect button and connect socket on page load
    Signed-off-by: tesla59 <nishant@heim.id>

--- src/app/meet/page.tsx
+++ src/app/meet/page.tsx
@@ -51,25 +51,63 @@ export default function Meet() {
   useEffect(() => {
     const id = initiateClientId();
     setClientID(id);
+
+    const ws = new WebSocket("ws://localhost:8080/ws");
+    setSocket(ws);
+
+    ws.onopen = () => {
+      console.log("Connected to WebSocket");
+      ws.send(JSON.stringify({ type: "identity", id: id }));
+    };
+
+    ws.onclose = () => {
+      ws.send(JSON.stringify({ type: "disconnected" }));
+      console.log("WebSocket connection closed");
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    ws.onmessage = (event) => {
+      try {
+        const parsedData = JSON.parse(event.data);
+        console.log("Received JSON Parsed as:", parsedData);
+
+        switch (parsedData.type) {
+          case "message":
+            handleReceiveMessage(event);
+            break;
+          case "matched":
+            setMessages([]);
+            setTargetID(parsedData.client_id);
+            break;
+          case "disconnected":
+            setMessages([]);
+            setTargetID("");
+            break;
+          default:
+            console.warn("Unknown message type:", parsedData.type);
+        }
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+    };
+
     return () => {
-      if (socket) {
+      if (socket && socket.readyState === WebSocket.OPEN) {
         socket.send(JSON.stringify({ type: "disconnect" }));
         socket.close();
       }
     };
-  }, [socket]);
+  }, []);
   return (
     <section className="h-screen w-screen flex flex-col px-6">
       <ConnectionDetails clientID={clientID} targetID={targetID} />
       <div className="grid grid-cols-7 justify-between items-start w-full flex-grow bg-blue-100">
         <VideoCallPanel
           className="col-span-5"
-          clientID={clientID}
           socket={socket}
-          setSocket={setSocket}
-          setMessages={setMessages}
-          setTargetID={setTargetID}
-          handleReceiveMessage={handleReceiveMessage}
         />
         <ChatWindow
           className="col-span-2"
